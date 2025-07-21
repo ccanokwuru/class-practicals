d472388f23399296c00314436e6ab93f6f457417
ccanokwuru <cccanoks@gmail.com>
2025-07-21 12:16:04 +0100

    comments for orm and db

--- backend_classes/cjs/database.js
+++ backend_classes/cjs/database.js
@@ -1,6 +1,13 @@
+// dotenv
+// this library helps to read the .env file making the key value peer avaliable to the process.env of node
 require("dotenv").config();
+
+// sequelize a Object Relationship Mapping ORM for SQL databases
+// this will help with our database connection and activities
 const { Sequelize } = require("sequelize");
 
+// initializing a database iinstance with sequelize
+// value are gotten from the .env
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USERNAME,
@@ -12,5 +19,7 @@ const sequelize = new Sequelize(
   }
 );
 
+// default export of instance
 export default sequelize;
+// named / object export of instance
 export { sequelize };
--- backend_classes/cjs/index.js
+++ backend_classes/cjs/index.js
@@ -1,6 +1,9 @@
+// dotenv
+// this library helps to read the .env file making the key value peer avaliable to the process.env of node
 require("dotenv").config();
 const express = require("express");
 const app = express();
+// using the environment varible PORT
 const port = process.env.PORT;
 
 app.get("/", (req, res) => {
