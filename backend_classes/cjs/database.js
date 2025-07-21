07ffd0db2aa5df7e370cd21fecfafa8229c4f7cf
ccanokwuru <cccanoks@gmail.com>
2025-07-21 12:10:00 +0100

    sequelize setup

---
+++ backend_classes/cjs/database.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: DB_HOST,
+    dialect:
+      DB_DIALECT /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
+  }
+);
+
+export default sequelize;
+export { sequelize };
